92ec60305d69003c62db54eb1dbb9de0932b6e47
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2019-10-01 17:45:19 +0100

    Create AuthenticationBuilderExtensions.cs

---
+++ ChauffeurApiCORE/ChauffeurApiCORE/Extensions/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ChauffeurApiCORE.Settings;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ChauffeurApiCORE.Extensions
+{
+	public static class AuthenticationBuilderExtensions
+	{
+		public static AuthenticationBuilder AddApiKeyAuth(this AuthenticationBuilder builder, Action<ApiKeyAuthOptions> configureOptions)
+		{
+			return builder.AddScheme<ApiKeyAuthOptions, ApiKeyAuthHandler>(ApiKeyAuthOptions.DefaultScheme, configureOptions);
+		}
+	}
+}
