e4a1dbe744814dd9889cbbe605eaccbac5cc8293
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2019-10-02 16:32:16 +0100

    Controllers

---
+++ ChauffeurApiCORE/ChauffeurApiCORE/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ChauffeurApiCORE.Commands;
+using ChauffeurApiCORE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChauffeurApiCORE.Controllers
+{
+	[Route("api/customers")]
+	[ApiController]
+	public class CustomersController : ControllerBase
+    {
+        IChaufferDbContext context;
+        ICreateCustomerCommand createCustomerCommand;
+
+        public CustomersController(ICreateCustomerCommand createCustomerCommand, IChaufferDbContext context)
+        {
+            this.context = context;
+            this.createCustomerCommand = createCustomerCommand;
+        }
+
+        public IQueryable<Customer> GetAllCustomers()
+        {
+            return context.Customers.OrderByDescending(b => b.FirstName);
+        }
+
+		[Route("")]
+        public IQueryable<Customer> GetCustomers([FromRoute] string searchTerm)
+        {
+            return context.Customers
+                .Where(c => c.FirstName.Contains(searchTerm) || c.LastName.Contains(searchTerm))
+                .OrderByDescending(c => c.FirstName);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<ActionResult> Register(CustomerBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await createCustomerCommand.Execute(model);
+
+            return Ok();
+        }
+    }
+}
---
+++ ChauffeurApiCORE/ChauffeurApiCORE/Controllers/DriversController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ChauffeurApiCORE.Commands;
+using ChauffeurApiCORE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChauffeurApiCORE.Controllers
+{
+    [Route("api/Drivers")]
+    public class DriversController : ControllerBase
+    {
+        IChaufferDbContext context;
+        ICreateDriverCommand createDriverCommand;
+
+        public DriversController(ICreateDriverCommand createDriverCommand, IChaufferDbContext context)
+        {
+            this.context = context;
+            this.createDriverCommand = createDriverCommand;
+        }
+
+        public IQueryable<Driver> GetAllDrivers()
+        {
+            return context.Dirvers.OrderByDescending(dr => dr.LastBookingDate);
+        }
+
+        [Route("ActiveInactive")]
+        public IQueryable<Driver> GetActiveInactiveDrivers([FromRoute] bool isActive)
+        {
+            return context.Dirvers.Where(d => d.IsActive == isActive).OrderByDescending(dr => dr.LastBookingDate);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<ActionResult> Register(Driver model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await createDriverCommand.Execute(model);
+
+            return Ok();
+        }
+    }
+}
