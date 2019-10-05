e8dd526a637bff755edc260145d802813fbeb8a4
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2019-10-05 16:32:58 +0100

    small adjustments

--- ChauffeurApiCORE/ChauffeurApiCORE/Models/PhoneNumber.cs
+++ ChauffeurApiCORE/ChauffeurApiCORE/Models/PhoneNumber.cs
@@ -5,7 +5,6 @@ namespace ChauffeurApiCORE.Models
     public class PhoneNumber
     {
 		[Key]
-        public string Telephone { get; set; }
         public string MobilePhone { get; set; }
         public string OfficePhone { get; set; }
     }
